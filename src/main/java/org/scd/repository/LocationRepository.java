f6cb004fb25bb537dbc1a2e7cc61c07039d5888f
Unknown <ionut.tomoss@gmail.com>
2019-01-05 15:51:40 +0200

    Location repository added

---
+++ src/main/java/org/scd/repository/LocationRepository.java
@@ -0,0 +1,21 @@
+package org.scd.repository;
+
+import org.scd.model.Location;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import java.util.Optional;
+
+public interface LocationRepository extends CrudRepository<Location,Long>{
+
+    Location save(Location location);
+    
+   Optional <Location> findById(final Long id);
+
+   void deleteById(final Long id);
+
+
+
+
+}
