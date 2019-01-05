87c5dc5efbb1f37e153456daa2c6039e1d59eb83
Unknown <ionut.tomoss@gmail.com>
2019-01-05 15:26:05 +0200

    Location DTO added

---
+++ src/main/java/org/scd/model/dto/LocationDTO.java
@@ -0,0 +1,31 @@
+package org.scd.model.dto;
+
+public class LocationDTO {
+
+    private double latitude;
+    private double longitude;
+
+    public LocationDTO() {
+    }
+
+    public LocationDTO(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
